2a927fa1f5a6b9791f79638f09fef0e41b33a40e
muziwenwu <li296641798@gmail.com>
2023-06-08 19:26:07 +0800

    步骤7 自动化反向传播

--- test.py
+++ test.py
@@ -2,10 +2,15 @@ from aa import *
 
 A = Square()
 B = Exp()
+C = Square()
+
 x = Variable(np.array(0.5))
 
 y0 = A(x)
 y1 = B(y0)
-y2 = A(y1)
+y2 = C(y1)
 
-print(y2.data)
+y2.grad = np.array(1.0)
+y2.backward()
+
+print(x.grad)
